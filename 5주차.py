98629a49755caf855a9eee75e68c865d5ca4e488
cms04220 <cms04220@naver.com>
2024-04-05 15:29:38 +0900

    2118064 최민석

---
+++ 5주차.py
@@ -0,0 +1,11 @@
+socre = input("학생들의 점수를 입력하세요: ")
+cs_socre = int(socre)
+
+if cs_socre >= 71 :
+    print("A")
+elif cs_socre >= 41 :
+    print("B")
+elif cs_socre >= 11 :
+    print("C")
+elif cs_socre <= 10 :
+    print("D")
